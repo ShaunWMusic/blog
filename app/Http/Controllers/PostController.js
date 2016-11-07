60353f0b34c4f853404fb21853c7dc0ec778f498
“Shaun <shaunwmusic@gmail.com>
2016-11-07 11:43:11 -0600

    Index page

---
+++ app/Http/Controllers/PostController.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const Post = use('App/Model/Post');
+
+class PostController {
+    //List of all results
+    //GET - /
+  * index(request, response) {
+    const posts = yield Post.all();
+
+    yield response.sendView('posts.index', {
+      posts: posts.toJSON()
+    });
+    //
+}
+
+  //Show the user a form to create a new Post
+  //GET - /create
+  * create(request, response) {
+    //
+  }
+
+  //Save a new Post to the database
+  //POST
+  * store(request, response) {
+    //
+  }
+
+  * show(request, response) {
+    //
+  }
+
+  * edit(request, response) {
+    //
+  }
+
+  * update(request, response) {
+    //
+  }
+
+  * destroy(request, response) {
+    //
+  }
+
+}
+
+module.exports = PostController
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -18,25 +18,4 @@
 const Route = use('Route');
 const Post = use('App/Model/Post');
 
-Route.get('/', function * (request, response) {
-  //Create a blog post with a set of data
-  const p = yield Post.create({
-    title: 'My first blog post',
-    author: 'Ryan',
-
-    slug: 'my-first-blog-post',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-
-
-    post_date: new Date(),
-    //"updated_at" and "create_at" will be auto-populated
-    //by adonis and lucid
-  });
-
-  response.send(p);
-
-  //Gets all rows from the "posts"
-  // const data = yield Database.table('posts').select('*');
-  //
-  // response.send(data);
-});
+Route.get('/', 'PostController.index');
